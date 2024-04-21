f09a89005cf69c393aa828b713fb7b24a843e85c
elanurimirgi <116424565+elanurimirgi@users.noreply.github.com>
2024-04-21 22:32:45 +0300

    Create bank_account.rb

---
+++ bank_account.rb
@@ -0,0 +1,31 @@
+class BankAccount
+    attr_reader :owner, :account_num
+    attr_accessor :balance
+
+    def initialize(owner, account_number, balance)
+        @owner = owner
+        @account_number = account_number
+        @balance = balance
+      end
+
+    def deposit(money)
+        @balance += money
+        puts "About #{money} was made. current balance: #{@balance}"
+    end
+
+    def withdraw(money)
+        if money > @balance
+            puts "insufficient balance"
+        else
+            @balance -= money
+            puts "About #{money} was withdrawn. Current balance: #{@balance}"
+        end
+    end
+end
+
+bank_user = BankAccount.new("ela", 1223456, 100)
+puts "#{bank_user.owner}'s account: #{bank_user.account_num}, Balance: #{bank_user.balance} TL"
+bank_user.deposit(500)
+bank_user.withdraw(200)
+
+# My goal in this exercise is to learn the basics of oop
